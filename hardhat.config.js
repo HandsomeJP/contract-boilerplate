d103727b120845ac18a355b0d2a4a70f231f0294
takaursa <jp0555251@students.mclennan.edu>
2022-06-27 22:04:37 -0400

    add goerli testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,20 @@
 require("@nomiclabs/hardhat-waffle");
+const dotenv = require('dotenv');
+dotenv.config()
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const GOERLI_PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${GOERLI_PRIVATE_KEY}`]
+    }
+  }
 };
